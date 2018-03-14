b61657694b7f1a1710f526ec5043669688739b39
Moritz Freyburger <moritz.freyburger@gmx.de>
2018-03-14 11:05:33 +0100

    Nice and clean admin elevation

--- Server/PiFormance.Server/AutostartProvider.cs
+++ Server/PiFormance.Server/AutostartProvider.cs
@@ -1,14 +1,21 @@
 ﻿namespace PiFormance.Server
 {
 	using System;
+	using System.Deployment.Application;
+	using Core.Standard.Extensions;
 	using Microsoft.Win32;
 
 	public static class AutostartProvider
 	{
-		// TODO: NEEDS TO BE CHANGED TO DEPLOYMENT PATH
-		// CURRENTLY NOT WORKING
-		private static readonly string StartPath = Environment.GetFolderPath(Environment.SpecialFolder.Programs) +
-		                                           @"\Moritz\PiFormance\PiFormance.Server.appref-ms";
+		//private const string TaskName = "PiFormance";
+
+		private static readonly string StartPath = Environment.GetFolderPath(Environment.SpecialFolder.Programs)
+		                   + @"\Moritz\PiFormance\PiFormance.Server.appref-ms";
+
+		//private static readonly string StartPath = Environment.GetFolderPath(Environment.SpecialFolder.Programs) +
+		//										   @"\" +
+		//                                           System.Security.Principal.WindowsIdentity.GetCurrent().Name +
+		//										   @"\PiFormance\PiFormance.Server.appref-ms";
 
 		private static readonly RegistryKey AppKey = Registry.CurrentUser.OpenSubKey(
 			@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run",
@@ -16,10 +23,32 @@
 
 		public static void RunOnWindowsStartup()
 		{
-			//if (AppKey.GetValue("PiFormance.Server").Cast<string>() != StartPath)
+			if (ApplicationDeployment.IsNetworkDeployed)
+			{
+				if (ApplicationDeployment.CurrentDeployment.IsFirstRun)
+				{
+					if (AppKey.GetValue("PiFormance.Server").Cast<string>() != StartPath)
+					{
+						AppKey.SetValue("PiFormance.Server", StartPath);
+					}
+				}
+			}
+				
+			//var taskService = TaskService.Instance;
+
+			//if (taskService.RootFolder.EnumerateTasks().Any(x => x.Name == TaskName))
 			//{
-			//	AppKey.SetValue("PiFormance.Server", StartPath);
+			//	return;
 			//}
+
+			//var taskDefinition = taskService.NewTask();
+
+			//taskDefinition.Principal.RunLevel = TaskRunLevel.Highest;
+			//taskDefinition.Triggers.Add(new LogonTrigger());
+			//taskDefinition.Actions.Add(new ExecAction(StartPath));
+
+			//taskService.RootFolder.RegisterTaskDefinition(TaskName, taskDefinition);
+
 		}
 	}
 }
--- Server/PiFormance.Server/Program.cs
+++ Server/PiFormance.Server/Program.cs
@@ -1,6 +1,9 @@
 ﻿namespace PiFormance.Server
 {
 	using System;
+	using System.Diagnostics;
+	using System.IO;
+	using System.Security.Principal;
 	using System.Threading;
 	using Console;
 
@@ -8,6 +11,29 @@
 	{
 		private static void Main(string[] args)
 		{
+			if (!IsRunAsAdministrator())
+			{
+				var test = Process.GetCurrentProcess().ProcessName;
+
+				ProcessStartInfo proc = new ProcessStartInfo
+				{
+					UseShellExecute = true,
+					WorkingDirectory = Environment.CurrentDirectory,
+					Verb = "runas"
+				};
+				proc.FileName = Path.Combine(proc.WorkingDirectory, test) + ".exe";
+
+				try
+				{
+					Process.Start(proc);
+				}
+				catch (Exception)
+				{
+				}
+
+				Environment.Exit(1);
+			}
+
 			if (true)
 			{
 				new ConsolePresenter(new ConsoleNativeMethods()).ShowConsole();
@@ -31,6 +57,14 @@
 					Thread.Sleep(100);
 				}
 			}
+		}
+
+		private static bool IsRunAsAdministrator()
+		{
+			var wi = WindowsIdentity.GetCurrent();
+			var wp = new WindowsPrincipal(wi);
+
+			return wp.IsInRole(WindowsBuiltInRole.Administrator);
 		}
 	}
 }
