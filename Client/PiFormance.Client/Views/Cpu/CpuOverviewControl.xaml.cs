8acc35dcad1b701b640d69054ab239f784863590
Moritz Freyburger <moritz.freyburger@gmx.de>
2018-04-01 14:54:42 +0200

    Changed the look of the overview tab

---
+++ Client/PiFormance.Client/Views/Cpu/CpuOverviewControl.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PiFormance.Client.Views.Cpu
+{
+	using Windows.UI.Xaml.Controls;
+
+	public sealed partial class CpuOverviewControl : UserControl
+	{
+		public CpuOverviewControl()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ Client/PiFormance.Client/Views/Gpu/GpuOverviewControl.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PiFormance.Client.Views.Gpu
+{
+	using Windows.UI.Xaml.Controls;
+
+	public sealed partial class GpuOverviewControl : UserControl
+	{
+		public GpuOverviewControl()
+		{
+			InitializeComponent();
+		}
+	}
+}
